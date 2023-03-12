5f13cf1112642366d5e8a0a120114220509f2ffa
黄诃华 <3116725220@qq.com>
2023-03-12 21:37:40 +0800

    feat(com/child/util/orm): 新建一个封装数据的类

---
+++ topviewassesment1-maven/src/main/java/com/child/util/orm/ForPreparedStatement.java
@@ -0,0 +1,31 @@
+package com.child.util.orm;
+
+import java.sql.Connection;
+
+/**
+ * 用于{@link SqlHandler#sqlHandler(ForPreparedStatement, Object)}，
+ * 将多参数封装到一个实体类中，利于维护。<br/>
+ */
+public class ForPreparedStatement {
+    /**
+     * 指定数据源的连接资源
+     */
+    private Connection connection;
+
+    /**
+     * 指定映射文件中的SQL
+     */
+    private String prototypeSql;
+
+    public ForPreparedStatement(Connection connection, String prototypeSql) {
+        this.connection = connection;
+        this.prototypeSql = prototypeSql;
+    }
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public String getPrototypeSql() {
+        return prototypeSql;
+    }
+}
